6d24a8b51270cd07e0cb384163eb7d71d62e8ed9
Rajanikant Deshmukh <rajanikantdeshmukh@gmail.com>
2015-08-12 04:15:53 +0530

    Minor changes.

--- app/src/main/java/com/myapp/blubot/fragments/BTConnectionFragment.java
+++ app/src/main/java/com/myapp/blubot/fragments/BTConnectionFragment.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -24,13 +25,13 @@ import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnItemClick;
 
 public class BTConnectionFragment extends Fragment {
 
     private static final String LOGCAT = BTConnectionFragment.class.getName();
 
     static ArrayList<BluetoothDevice> devices = new ArrayList<>();
-    //private ArrayAdapter<String> adapter;
     private BluetoothDeviceAdapter adapter;
 
     @Bind(R.id.fragment_bt_connection_list)
@@ -60,21 +61,11 @@ public class BTConnectionFragment extends Fragment {
         return rootView;
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (ConnectionFragmentListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement ConnectionFragmentListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    @OnItemClick(R.id.fragment_bt_connection_list)
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+    {
+        BluetoothDevice device = devices.get(position);
+        mListener.saveDeviceStartControl(device);
     }
 
     private void discoverDevices() {
@@ -88,15 +79,9 @@ public class BTConnectionFragment extends Fragment {
         // Start discovery // Need BLUETOOTH_ADMIN permission
         boolean discoveryStarted = mListener.startBTDeviceDiscovery();
         if(!discoveryStarted)
-            Toast.makeText(getActivity(), "Blutooth discovery failed.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Bluetooth discovery failed.", Toast.LENGTH_LONG).show();
         Log.e(LOGCAT, "Discovery started");
-        Toast.makeText(getActivity(),"",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        getActivity().unregisterReceiver(bluetoothReceiver);
+        Toast.makeText(getActivity(),"Discovery started",Toast.LENGTH_SHORT).show();
     }
 
     // Create a BroadcastReceiver for discovery of bluetooth devices
@@ -114,8 +99,33 @@ public class BTConnectionFragment extends Fragment {
                 Toast.makeText(getActivity(),"",Toast.LENGTH_SHORT).show();
             }else {
                 android.util.Log.e(LOGCAT, "Action : " + action + " is not bluetooth related.");
-                Toast.makeText(getActivity(),"",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(),"Action : " + action + " is not bluetooth related.",Toast.LENGTH_SHORT).show();
             }
         }
     };
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (ConnectionFragmentListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement ConnectionFragmentListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(bluetoothReceiver);
+    }
 }
