19aa83c31bb104c70e4e9f96fedcb01a752f622b
Rajanikant Deshmukh <rajanikantdeshmukh@gmail.com>
2015-08-30 14:33:23 +0530

    Add bluetooth write functions.
    Buttons in control layout linked with ControlActivity.
    OnClick Listeners set to enable actual data output.

--- app/src/main/java/com/myapp/blubot/ControlActivity.java
+++ app/src/main/java/com/myapp/blubot/ControlActivity.java
@@ -1,30 +1,95 @@
 package com.myapp.blubot;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
+import java.io.IOException;
 import java.io.OutputStream;
+
+import at.markushi.ui.CircleButton;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ControlActivity extends AppCompatActivity {
 
     OutputStream outputStream;
+    @Bind(R.id.control_board_button_a)
+    CircleButton controlBoardButtonA;
+    @Bind(R.id.control_board_button_up)
+    CircleButton controlBoardButtonUp;
+    @Bind(R.id.control_board_button_b)
+    CircleButton controlBoardButtonB;
+    @Bind(R.id.control_board_button_left)
+    CircleButton controlBoardButtonLeft;
+    @Bind(R.id.control_board_button_close)
+    CircleButton controlBoardButtonClose;
+    @Bind(R.id.control_board_button_right)
+    CircleButton controlBoardButtonRight;
+    @Bind(R.id.control_board_button_c)
+    CircleButton controlBoardButtonC;
+    @Bind(R.id.control_board_button_down)
+    CircleButton controlBoardButtonDown;
+    @Bind(R.id.control_board_button_d)
+    CircleButton controlBoardButtonD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+        ButterKnife.bind(this);
 
         outputStream = BluBot.getOutputStream();
-        if(outputStream==null)
-        {
+
+        if (outputStream == null) {
             // TODO Handle exception here
+
         }
     }
 
+    @OnClick(R.id.fragment_bt_unavailable_github_link)
+    public void onControlButtonClick(View view)
+    {
+        int viewId = view.getId();
+        if(outputStream==null)
+            return;
+        try {
+            switch (viewId) {
+                case R.id.control_board_button_a:
+                    outputStream.write(0x0a);
+                    break;
+                case R.id.control_board_button_b:
+                    outputStream.write(0x0b);
+                    break;
+                case R.id.control_board_button_c:
+                    outputStream.write(0x0c);
+                    break;
+                case R.id.control_board_button_d:
+                    outputStream.write(0x0d);
+                    break;
+                case R.id.control_board_button_up:
+                    outputStream.write(0x01);
+                    break;
+                case R.id.control_board_button_down:
+                    outputStream.write(0x02);
+                    break;
+                case R.id.control_board_button_left:
+                    outputStream.write(0x03);
+                    break;
+                case R.id.control_board_button_right:
+                    outputStream.write(0x04);
+                    break;
+                case R.id.control_board_button_close:
+                    outputStream.write(0x05);
+                    break;
+                default:
+            }
+        }catch (IOException e)
+        {
 
+        }
+    }
 
 
     /*@Override
